e3e3543bfa86fce969bdc6c029a8800542b5157c
Rudradev78 <rudranarayanmohapatra87878@gmail.com>
2025-11-20 16:48:51 +0530

    Updated App.jsx and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Meet from './pages/Meet';
 import Chat from './pages/Chat';
 
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/meet" element={<Meet />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
